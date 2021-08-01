b4e0860e5e44c00f727ced57f37ffc7308cf9988
Shay Ronen <shayronen138@gmail.com>
2021-08-01 11:20:29 +0300

    routing, redux boilerplate, post and get data from firebase and login system

---
+++ App.js
@@ -0,0 +1,94 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import * as firebase from "firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { View, Text } from "react-native";
+
+import Landing from "./components/auth/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Main from "./components/Main";
+import Add from "./components/main/Add";
+import { ENV } from "./envVars";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const firebaseConfig = ENV;
+
+if (firebase.default.apps.length === 0) {
+  firebase.default.initializeApp(firebaseConfig);
+}
+
+const Stack = createStackNavigator();
+export default function App() {
+  const [loaded, setLoaded] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    firebase.default.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        setLoaded(true);
+        setLoggedIn(false);
+      } else {
+        setLoaded(true);
+        setLoggedIn(true);
+      }
+    });
+  });
+
+  if (!loaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+
+  if (!loggedIn) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen
+            name="Landing"
+            component={Landing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ headerShown: true }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: true }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen
+            name="Main"
+            component={Main}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Add"
+            component={Add}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
---
+++ components/Main.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import { View, Text } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { fetchUser } from "../redux/actions/index";
+import Feed from "./main/Feed";
+import Profile from "./main/Profile";
+
+const Tab = createMaterialBottomTabNavigator();
+
+const EmptyScreen = () => null;
+
+const Main = (props) => {
+  useEffect(() => {
+    props.fetchUser();
+  }, []);
+
+  return (
+    <Tab.Navigator initialRouteName="Feed" labeled={false}>
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AddContainer"
+        component={EmptyScreen}
+        listeners={({ navigation }) => ({
+          tabPress: (event) => {
+            event.preventDefault();
+            navigation.navigate("Add")
+          },
+        })}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-circle"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const mapSateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+});
+const mapDispatchProps = (dispatch) =>
+  bindActionCreators({ fetchUser }, dispatch);
+
+export default connect(mapSateToProps, mapDispatchProps)(Main);
